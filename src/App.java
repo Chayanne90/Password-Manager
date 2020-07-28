430cb2ccf6e2570eba62b5347719a4a67524cc4b
Chayanne Paulino <chayanne@Chayannes-MacBook-Pro.local>
2020-07-27 23:06:19 -0400

    Application it's running from Gui

--- src/App.java
+++ src/App.java
@@ -2,10 +2,14 @@
 * By Chayanne
 * */
 
-public class App {
+import javax.naming.ldap.ManageReferralControl;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
+public class App  {
     public static void main (String [] args) {
-
-
+        PwdManagerGUI gui = new PwdManagerGUI();
     }
 }
